8316e437500e6406e12ce219a0801c50dbc63155
Gabriel Mania <maniagp@proton.me>
2024-11-22 23:32:36 +0800

    Fix list item ordering, go back to html5 in FT

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -3,7 +3,12 @@ from unittest import skip
 from django.test import TestCase
 from django.utils.html import escape
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR,
+    EMPTY_ITEM_ERROR,
+    ExistingListItemForm,
+    ItemForm,
+)
 from lists.models import Item, List
 
 
@@ -94,10 +99,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         mylist = List.objects.create()
         response = self.client.get(f"/lists/{mylist.id}/")
-        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         Item.objects.create(list=list1, text="textey")
@@ -106,10 +110,14 @@ class ListViewTest(TestCase):
             data={"text": "textey"},
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, "list.html")
         self.assertEqual(Item.objects.all().count(), 1)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
 
 
 class NewListTest(TestCase):

8b871371e1106be355a65ebe161a0d7cdb9f124b
Gabriel Mania <maniagp@proton.me>
2024-11-24 00:23:04 +0800

    Initial implementation of authentication

---
+++ src/accounts/authentication.py
@@ -0,0 +1,20 @@
+from accounts.models import Token, User
+
+
+class PasswordlessAuthenticationBackend:
+    def authenticate(self, request, uid):
+        token = None
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            if token is not None:
+                return User.objects.create(email=token.email)
+        except Token.DoesNotExist:
+            return None
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None

3d9a52d068f7733be82fad4d7c29822cbb2212f2
Gabriel Mania <maniagp@proton.me>
2024-11-24 16:17:58 +0800

    Implement logging out users

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from accounts import views
@@ -5,4 +6,5 @@ from accounts import views
 urlpatterns = [
     path("login", views.login, name="login"),
     path("send_login_email", views.send_login_email, name="send_login_email"),
+    path("logout", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
